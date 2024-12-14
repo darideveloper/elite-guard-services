84c63920c167dde9dee1931654fcb141c2e49468
daridev <darideveloper@gmail.com>
2024-12-13 18:30:24 -0600

    feat (jazzmin): hide secondary models

--- project/settings.py
+++ project/settings.py
@@ -202,7 +202,13 @@ JAZZMIN_SETTINGS = {
     "hide_apps": [],
 
     # Hide these models when generating side menu (e.g auth.user)
-    "hide_models": [],
+    "hide_models": [
+        "employees.Colony",
+        "employees.Municipality",
+        "employees.MaritalStatus",
+        "employees.Status",
+        "employees.Bank",
+    ],
 
     # List of apps (and/or models) to base side menu ordering off of
     # (does not need to contain all apps/models)
