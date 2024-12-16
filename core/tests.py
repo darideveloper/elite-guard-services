46941bac01eff24c4a5ad76a3395d6ee8488a9e7
daridev <darideveloper@gmail.com>
2024-12-16 08:05:47 -0600

    feat (urls): direct / to /admin

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RedirectsTestCase(TestCase):
+    def test_home_redirect_admin(self):
+        """ Test redirect to admin when accessing the home page """
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/admin/')
