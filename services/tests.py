2ad462e38aa3495e7c89f11d331678ac0c6ca3b8
daridev <darideveloper@gmail.com>
2024-12-24 16:17:20 -0600

    test (services): AgreementModelTest

--- services/tests.py
+++ services/tests.py
@@ -1,10 +1,45 @@
-from datetime import datetime
-
 from django.test import TestCase
 from django.core.management import call_command
 
 from utils import test_data
 
+
+class AgreementModelTest(TestCase):
+    """ Validate custom methods in model """
+    
+    def setUp(self):
+        
+        # Create initial data
+        call_command("apps_loaddata")
+        
+        # Create Agreement
+        self.agreement = test_data.create_agreement()
+        self.employee = test_data.create_employee()
+        self.service = test_data.create_service(
+            agreement=self.agreement,
+            employee=self.employee
+        )
+        
+    def test_get_services(self):
+        """ Validate get_services method """
+        
+        # Get services
+        services = self.agreement.get_services()
+        
+        # Validate services
+        self.assertEqual(services.count(), 1)
+        self.assertEqual(services[0], self.service)
+        
+    def test_get_employees(self):
+        """ Validate get_employees method """
+        
+        # Get employees
+        employees = self.agreement.get_employees()
+        
+        # Validate employees
+        self.assertEqual(employees.count(), 1)
+        self.assertEqual(employees[0], self.employee)
+        
 
 class AgreementAdminTest(TestCase):
     """ Test custom features in admin/agreement """
